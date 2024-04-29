6c3ce266e66eda41df9554801bd757d47e0e48ba
wiserdivisor <dat0user@gmail.com>
2024-04-29 23:22:38 +0530

    revamp

--- src/component/home.js
+++ src/component/home.js
@@ -9,28 +9,45 @@ function Home({ children }) {
   return(
     <div className="body">
 
-      <GraphBox>
-      </GraphBox>
+      <GraphBox />
 
       <div className="intro-container">
         <img className="surya" src={dp}></img>
         <div className="intro">
           Hello, I'm <p className="divine-inline">Vignesh</p><br/>
           I enjoy solving problems with software/hardware.<br/>
+          I primarily speak C++17 and Python.<br/>
+          I use frames like: Qt || React || Node.
+          {/*
           Got a hard problem? Tell me more.<br/>
-          Mail: vignesh@vreddy.org
+          Mail: vignesh@vreddy.org*/}
+        </div>
+      </div>
+      <div className="question">
+        LOOKING FOR A FULLSTACKER?
+      </div>
+      <div className="brag-container">
+        <div className="eclipse-container">
+          <div className="eclipse">
+            <div className="undivine-inline">I can handle your Entire Solution. End to End.</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="portrait-card">
+            AUT<br/>OMA<br/>TION
+          </div>
+          <div className="portrait-card">
+            FRO<br/>NTE<br/>ND
+          </div>
+          <div className="portrait-card">
+            BAC<br/>KE<br/>ND
+          </div>
+          <div className="portrait-card">
+            DAT<br/>AB<br/>ASE
+          </div>
         </div>
       </div>
 
-      <div className="brag-container">
-        <p className="oneliner">I'm as fullstack as one can get.</p>
-        <div className="row">
-          <div className="portrait-card"></div>
-          <div className="portrait-card"></div>
-          <div className="portrait-card"></div>
-          <div className="portrait-card"></div>
-        </div>
-      </div>
 
     </div>
   );
